d83b267d7eb423e9eb918ea8ff1c8ac7726ed8fb
TWKuoNW <jp6jp642311@gmail.com>
2024-03-02 13:44:09 +0800

    update V3

--- TemperatureSensorInterface/call2.py
+++ TemperatureSensorInterface/call2.py
@@ -1,14 +1,14 @@
-from temp_sensor_interface_V2 import SensorType, SensorReader
+from temp_sensor_interface_V3 import SensorReader
 import struct
 import keyboard
 
 sensor_reader = SensorReader()
 
 while(True):
-    v = sensor_reader.read_value(SensorType.HUMIDITY)
-    v = struct.unpack(">If", v)
-    print(f"v: {v}%")
-    
+    data = sensor_reader.read_value("HUMIDITY")
+    data = struct.unpack("<If", data)
+    print("data:", data)
+
     if keyboard.is_pressed('esc'):
         break
 
